8d01570a9821359ddc1dc7f58df748bc00219f4c
Mikhail Metrikin <metrikin@gmail.com>
2017-04-29 21:18:37 -0700

    Fix

--- redditTest.js
+++ redditTest.js
@@ -1,7 +1,10 @@
 let snoowrap = require('snoowrap');
 const redditConfig = require('./config.js');
 
-
-console.log(redditConfig);
 const reddit = new snoowrap(redditConfig);
+
+reddit.getSubreddit('videos');
+//.getHot().then(console.log); //.map(post => post.title).then(console.log);
+
+reddit.
 
