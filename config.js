ae56702172179baffce5f5a40b3f6db32a0bb09b
Mikhail Metrikin <metrikin@gmail.com>
2017-05-02 00:51:57 -0700

    Exercise Compeleted

--- config.js
+++
@@ -1,8 +0,0 @@
-const redditConfig =
-{ userAgent: "Test Reddit App",
-  clientId: 'X5vNa2UcCGvftg' ,
-  clientSecret: 'lnmF9gr_c3VLcPYfYmpeqnq3GKQ',
-  username: 'volkriotTest',
-  password: 'basset12' };
-
-module.exports = redditConfig;
--- redditTest.js
+++ redditTest.js
@@ -3,8 +3,5 @@ const redditConfig = require('./config.js');
 
 const reddit = new snoowrap(redditConfig);
 
-reddit.getSubreddit('videos');
+let videosHot = reddit.getSubreddit('videos').getHot().then(console.log);
 //.getHot().then(console.log); //.map(post => post.title).then(console.log);
-
-reddit.
-
